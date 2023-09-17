0645b8c6aa41c61b75130e50988ed48d413582d2
Taisei Klasen <taiseiklasen@gmail.com>
2023-09-17 12:13:45 +0900

    Add prettier class sorting for tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css"
 function App() {
   return (
     <>
-      <div className="intro flex items-center justify-center h-screen w-screen">
+      <div className="intro flex items-center justify-center h-screen w-screen bg-white fixed inset-0">
         <img
           src={cotenLogo}
           className="h-1/3 w-1/3 animate-slide-out-fwd-center"
