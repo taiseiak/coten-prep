85115d23dd40fb5c1a582ccbb362b067dc447606
Taisei Klasen <taiseiklasen@gmail.com>
2023-09-17 13:07:07 +0900

    Add basic page for yozora

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,31 @@
-import cotenLogo from "/cotenLogo.svg"
-import "./App.css"
+import cotenLogo from "/cotenLogo.svg";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="intro flex items-center justify-center h-screen w-screen bg-white fixed inset-0">
-        <img
-          src={cotenLogo}
-          className="h-1/3 w-1/3 animate-slide-out-fwd-center"
-          alt="Coten logo"
-        />
-      </div>
+      <main>
+        <section className="intro fixed inset-0 flex h-screen w-screen animate-slide-out-top items-center justify-center bg-white">
+          <img
+            src={cotenLogo}
+            className="h-1/3 w-1/3 animate-slide-out-fwd-center"
+            alt="Coten logo"
+          />
+        </section>
+        <section className="bg-white dark:bg-slate-700">
+          <div className="mx-auto grid max-w-screen-xl px-4 py-8">
+            <div className="mr-auto place-self-center">
+              <h1 className="max-w-2xl text-4xl font-extrabold leading-none text-sky-400 dark:text-slate-200">
+                Yozora
+              </h1>
+              <p className="max-w-2xl">See a sky full of memories</p>
+              <input type="text" placeholder="Search" />
+            </div>
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,8 @@ export default {
       animation: {
         "slide-out-fwd-center":
           "slide-out-fwd-center 1s cubic-bezier(0.550, 0.085, 0.680, 0.530) both",
+        "slide-out-top":
+          "slide-out-top 200ms cubic-bezier(0.550, 0.085, 0.680, 0.530) 1s both",
       },
       keyframes: {
         "slide-out-fwd-center": {
@@ -18,8 +20,18 @@ export default {
             opacity: 0,
           },
         },
+        "slide-out-top": {
+          "0%": {
+            transform: "translateY(0)",
+            opacity: 1,
+          },
+          "100%": {
+            transform: "translateY(100%)",
+            opacity: 0,
+          },
+        },
       },
     },
   },
   plugins: [],
-}
+};
