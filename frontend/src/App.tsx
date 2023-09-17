7036560535cc9348bc01f89d12d510e759a1ba40
Taisei Klasen <taiseiklasen@gmail.com>
2023-09-17 11:45:49 +0900

    Add tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css"
 function App() {
   return (
     <>
-      <img src={cotenLogo} className="logo" alt="Coten logo" />
+      <img src={cotenLogo} className="center-logo" alt="Coten logo" />
     </>
   )
 }
